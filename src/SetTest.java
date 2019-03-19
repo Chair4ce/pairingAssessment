c26363836d11ad6c3e4956ca56875cda9bc09103
Jason Rimer <jasonmrimer@gmail.com>
2019-03-19 05:12:07 -1000

    Blank slate assessment setup

---
+++ src/SetTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class SetTest {
+
+}
