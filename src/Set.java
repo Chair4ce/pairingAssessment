ca9bd7c2d10e2f931f30f72096e32840f7cc8420
Jason Rimer <jasonmrimer@gmail.com>
2019-03-19 05:16:35 -1000

    Interbview with Gus Morcate on 20190318

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,25 @@
 public class Set {
+    private Object[] container = new Object[10];
+    private boolean empty = true;
+    private int count = 0;
 
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public void add(Object object) {
+        container[count] = object;
+        count = count + 1;
+        empty = false;
+    }
+
+    public boolean contains(Object object) {
+        boolean iHaveIt = false;
+        for (int index = 0; index < 10; index++) {
+            if (container[index] == object) {
+                iHaveIt = true;
+            }
+        }
+        return iHaveIt;
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -1,3 +1,5 @@
+Gus Morcate
+
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
@@ -5,5 +7,38 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
+    @Test
+    public void newSetIsEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty());
+    }
 
+    @Test
+    public void IsNotEmptyAfterAddingChocolate() {
+        Set jar = new Set();
+        Object chocolate = new Object();
+        jar.add(chocolate);
+        assertFalse(jar.isEmpty());
+    }
+
+    @Test
+    public void SetIntegriy() {
+        Set tackleBox = new Set();
+        Object fishingHook = new Object();
+        tackleBox.add(fishingHook);
+        assertTrue(tackleBox.contains(fishingHook));
+    }
+
+    @Test
+    public void SetHasManyObjectsAfterAdd() {
+        Set tackleBox = new Set();
+        Object fishingHook = new Object();
+        Object sinker = new Object();
+
+        tackleBox.add(fishingHook);
+        tackleBox.add(sinker);
+
+        assertTrue(tackleBox.contains(fishingHook));
+        assertTrue(tackleBox.contains(sinker));
+    }
 }
