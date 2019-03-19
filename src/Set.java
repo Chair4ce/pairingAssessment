d66df682d457fb6e480ff808e3140e72bf0a62a7
Jason Rimer <jasonmrimer@gmail.com>
2019-03-19 11:56:28 -1000

    Interview with Derick Chandler 30190319

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,26 @@
 public class Set {
+    private boolean empty = true;
+    private Object[] container = new Object[10];
+    private int count = 0;
 
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public void add(Object object) {
+        container[count] = object;
+        count = count + 1;
+        empty = false;
+    }
+
+    public boolean contains(Object object) {
+        boolean doesContain = false;
+        for (int index = 0; index < count; index++) {
+
+            if (container[index] == object) {
+                doesContain = true;
+            }
+        }
+        return doesContain;
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -5,5 +5,39 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
+    @Test
+    public void NewSetIsEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty());
+    }
+
+    @Test
+    public void SetIsNotEmptyAfterAdd() {
+        Set subject = new Set();
+        Object hammer = new Object();
+        subject.add(hammer);
+        assertFalse(subject.isEmpty());
+    }
+
+    @Test
+    public void ShouldContainObjectAfterAdd() {
+        Set subject = new Set();
+        Object hammer = new Object();
+        subject.add(hammer);
+        assertTrue(subject.contains(hammer));
+    }
+
+    @Test
+    public void ShouldContainManyObjectsAfterAdd(){
+        Set subject = new Set();
+        Object hammer = new Object();
+        Object wrench = new Object();
+        subject.add(hammer);
+        subject.add(wrench);
+
+        assertTrue(subject.contains(hammer));
+        assertTrue(subject.contains(wrench));
+    }
+
 
 }
