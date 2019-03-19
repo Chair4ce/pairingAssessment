8a0c8c212e35a97e686a7b6077ab9b683e56d753
Jason Rimer <jasonmrimer@gmail.com>
2019-03-19 05:38:12 -1000

    Interview with John Schultz 30190318

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,34 @@
 public class Set {
+    private boolean empty = true;
+    private Object[] bucket = new Object[10];
+    private int position = 0;
 
+    public boolean isEmpty() {
+        return this.empty;
+    }
+
+    public void insert(Object object) {
+        this.bucket[this.position] = object;
+        this.position++;
+        this.empty = false;
+    }
+
+    public boolean contains(Object object) {
+        return this.indexOf(object) > -1;
+    }
+
+    public void remove(Object object) {
+        if (this.contains(object)) {
+            this.bucket[this.indexOf(object)] = null;
+        }
+    }
+
+    private int indexOf(Object object) {
+        for (int index = 0; index < this.position; index++) {
+            if (this.bucket[index] == object) {
+                return index;
+            }
+        }
+        return -1;
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -1,3 +1,5 @@
+John Schultz
+
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
@@ -5,5 +7,44 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
+    @Test
+    public void newSetIsEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty());
+    }
 
+    @Test
+    public void InsertObjectSetNotEmpty() {
+        Set subject = new Set();
+        Object object = new Object();
+        subject.insert(object);
+        assertFalse(subject.isEmpty());
+    }
+
+    @Test
+    public void canRetrieveObjectAfterInsert() {
+        Set subject = new Set();
+        Object object = new Object();
+        subject.insert(object);
+        assertTrue(subject.contains(object));
+    }
+
+    @Test
+    public void canInsertMultipleObjects() {
+        Set subject = new Set();
+        Object object1 = new Object();
+        Object object2 = new Object();
+        subject.insert(object1);
+        subject.insert(object2);
+        assertTrue(subject.contains(object1));
+        assertTrue(subject.contains(object2));
+    }
+
+    @Test
+    public void canRemoveObject() {
+        Set subject = new Set();
+        Object object1 = new Object();
+        subject.remove(object1);
+        assertFalse(subject.contains(object1));
+    }
 }
