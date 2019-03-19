70133015ca3263b45db93393dcb04c3ea0a6cfeb
Jason Rimer <jasonmrimer@gmail.com>
2019-03-19 09:58:58 -1000

    Interview with Ian Persaud 30190319

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,22 @@
 public class Set {
+    private Object[] container = new Object[10];
+    private int count = 0;
 
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public void add(Object object) {
+        container[count] = object;
+        count++;
+    }
+
+    public boolean contains(Object object) {
+        for (int index = 0; index < count; index++) {
+            if (container[index] == object) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -5,5 +5,48 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
+    @Test
+    public void NewSetIsEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty());
+    }
 
+    @Test
+    public void SetIsNotEmptyAfterAdd() {
+        Set subject = new Set();
+        Object object = new Object();
+        subject.add(object);
+        assertFalse(subject.isEmpty());
+    }
+
+    @Test
+    public void SetContainsObjectAfterAdd() {
+        Set subject = new Set();
+        Object object = new Object();
+        subject.add(object);
+        assertTrue(subject.contains(object));
+    }
+
+    @Test
+    public void SetShouldNotContainUnaddedObject() {
+        Set subject = new Set();
+        Object hammer = new Object();
+        Object nail = new Object();
+        subject.add(hammer);
+        assertFalse(subject.contains(nail));
+    }
+
+    @Test
+    public void SetCanAddManyObjects() {
+        Set subject = new Set();
+        Object hammer = new Object();
+        Object nail = new Object();
+
+        subject.add(hammer);
+        subject.add(nail);
+
+        assertFalse(subject.isEmpty());
+        assertTrue(subject.contains(hammer));
+        assertTrue(subject.contains(nail));
+    }
 }
