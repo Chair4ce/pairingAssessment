d021b27339e2e44d8e353f10cdfcb23aad733661
Jason Rimer <jasonmrimer@gmail.com>
2019-03-19 05:36:18 -1000

    Interview with Matthew Ramos 30190318

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,27 @@
 public class Set {
+    private boolean empty = true;
+    private Object[] possessions = new Object[10];
+    private int count = 0;
 
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public void insert(Object object) {
+        possessions[count] = object;
+        count++;
+        empty = false;
+    }
+
+    public void remove(Object object) {
+        possessions[0] = null;
+        empty = true;
+    }
+
+    public boolean possesses(Object object) {
+        if (possessions[0] == object) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -5,5 +5,38 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
+    @Test
+    public void newSetIsEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty());
+    }
 
+    @Test
+    public void setHasObjectAfterInsert() {
+        Set subject = new Set();
+        Object object = new Object();
+        subject.insert(object);
+        assertTrue(subject.possesses(object));
+    }
+
+    @Test
+    public void setDoesNotHaveObjectAfterRemove() {
+        Set subject = new Set();
+        Object object = new Object();
+        subject.insert(object);
+        subject.remove(object);
+        assertTrue(subject.isEmpty());
+        assertFalse(subject.possesses(object));
+    }
+
+    @Test
+    public void setHasMultipleObjectsAfterInsert() {
+        Set subject = new Set();
+        Object object1 = new Object();
+        Object object2 = new Object();
+        subject.insert(object1);
+        subject.insert(object2);
+        assertTrue(subject.possesses(object1));
+        assertTrue(subject.possesses(object2));
+    }
 }
