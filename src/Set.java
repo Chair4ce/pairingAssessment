1d0cc82cb74bcebbe459f305108a8d1119a63834
Jason Rimer <jasonmrimer@gmail.com>
2019-03-19 15:58:13 -1000

    Interview Kimberly Lewis 30190319

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,5 @@
 public class Set {
-
+    public boolean isEmpty() {
+        return true;
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -5,5 +5,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
-    
+    @Test
+    public void NewSetIsEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty())
+    }
 }
