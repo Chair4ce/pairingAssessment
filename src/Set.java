7f0b9268b99bc7382cd20acc4585dc403c1e7e06
Jason Rimer <jasonmrimer@gmail.com>
2019-03-20 05:40:38 -1000

    Interview with Eric Robinson 30190319

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,58 @@
 public class Set {
-    
+    private int containerSize = 2;
+    private Object[] container = new Object[containerSize];
+    private int count = 0;
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public void add(Object object) {
+        if (this.contains(object)) {
+            return;
+        }
+
+        if (count == containerSize) {
+            containerSize++;
+            Object[] newContainer = new Object[containerSize];
+            for (int i = 0; i < count; i++) {
+                newContainer[i] = container[i];
+            }
+            container = newContainer;
+        }
+        this.container[count] = object;
+        count++;
+    }
+
+    public boolean contains(Object object) {
+        for (int index = 0; index < count; index++) {
+            if (container[index].equals(object)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void remove(Object object) {
+        for (int i = 0; i < count; i++) {
+            if (container[i].equals(object)) {
+                container[i] = null;
+                count--;
+                return;
+            }
+        }
+//        boolean[] booleans = new boolean[count];
+//        for (int i = 0; i < count; i++) {
+//            booleans[i] = false;
+//            if (container[i].equals(object)) {
+//                booleans[i] = true;
+//            }
+//        }
+//
+//        for (int i = 0; i < count; i++) {
+//            if (!booleans[i]) {
+//
+//            }
+//        }
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -5,5 +5,82 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
+    @Test
+    public void NewSetIsEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty());
+    }
 
+    @Test
+    public void SetIsNotEmptyAfterInput() {
+        Set subject = new Set();
+        subject.add(new Object());
+        assertFalse(subject.isEmpty());
+    }
+
+    @Test
+    public void SetKeepsObjectOnInput() {
+        Set subject = new Set();
+        Object object = new Object();
+        subject.add(object);
+        assertTrue(subject.contains(object));
+    }
+
+    @Test
+    public void SetKeepsManyObjectsOnInput() {
+        Set subject = new Set();
+        Object object1 = new Object();
+        Object object2 = new Object();
+        subject.add(object1);
+        subject.add(object2);
+
+        assertTrue(subject.contains(object1));
+        assertTrue(subject.contains(object2));
+    }
+
+    @Test
+    public void SetKeepsArbitraryNumberOfObjects() {
+        Set subject = new Set();
+        int max = (int) (Math.random() * 10000);
+        for (int i = 0; i < max; i++) {
+            subject.add(i);
+        }
+
+        Object lastObject = new Object();
+        subject.add(lastObject);
+
+        assertTrue(subject.contains(0));
+        assertTrue(subject.contains((int) (max - 1) / 2));
+        assertTrue(subject.contains(max - 1));
+        assertTrue(subject.contains(lastObject));
+    }
+
+    @Test
+    public void SetShouldNotContainDuplicates() {
+        Set subject = new Set();
+        Object object = new Object();
+
+        subject.add(object);
+        subject.add(object);
+        subject.remove(object);
+
+        assertTrue(subject.isEmpty());
+    }
+
+    @Test
+    public void SetShouldFacilitateMultipleAddsAndRemoves() {
+        Set subject = new Set();
+        Object object1 = new Object();
+        Object object2 = new Object();
+        Object object3 = new Object();
+
+        subject.add(object1);
+        subject.add(object2);
+        subject.remove(object1);
+        subject.add(object3);
+
+        assertFalse(subject.contains(object1));
+        assertTrue(subject.contains(object2));
+        assertTrue(subject.contains(object3));
+    }
 }
