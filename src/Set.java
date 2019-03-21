506d5fab679d53079c33d5853a1333925aa21304
Jason Rimer <jasonmrimer@gmail.com>
2019-03-21 05:31:25 -1000

    Interview with Brad Schroeder 20190320

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,35 @@
 public class Set {
-    
+    private Object[] container = new Object[10];
+    private int objectCount = 0;
+
+    public boolean isEmpty() {
+        return objectCount == 0;
+    }
+
+    public void add(Object object) {
+        container[objectCount] = object;
+        objectCount++;
+    }
+
+    public void remove(Object object) {
+        int index = indexOf(object);
+        if (index > -1) {
+            container[index] = container[objectCount -1];
+            container[objectCount -1] = null;
+            objectCount--;
+        }
+    }
+
+    public boolean contains(Object object) {
+        return indexOf(object) > -1;
+    }
+
+    private int indexOf(Object object) {
+        for (int index = 0; index < objectCount; index++) {
+            if (container[index].equals(object)) {
+                return index;
+            }
+        }
+        return -1;
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
@@ -5,5 +6,71 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
+    private Set subject;
 
+    @Before
+    public void setup() {
+        subject = new Set();
+    }
+
+    @Test
+    public void NewSetIsEmpty() {
+        assertTrue(subject.isEmpty());
+    }
+
+    @Test
+    public void IsNotEmptyAfterAdd() {
+        subject.add(new Object());
+        assertFalse(subject.isEmpty());
+    }
+
+    @Test
+    public void IsEmptyAfterRemove() {
+        Object object = new Object();
+        subject.add(object);
+        subject.remove(object);
+        assertTrue(subject.isEmpty());
+    }
+
+    @Test
+    public void SetContainsObjectAfterAdd() {
+        Object object = new Object();
+        subject.add(object);
+        assertTrue(subject.contains(object));
+    }
+
+    @Test
+    public void SetShouldNotContainObjectAfterRemove() {
+        Object object = new Object();
+        subject.add(object);
+        subject.remove(object);
+        assertFalse(subject.contains(object));
+    }
+
+    @Test
+    public void AddManyItems() {
+        Object object1 = new Object();
+        Object object2 = new Object();
+        subject.add(object1);
+        subject.add(object2);
+
+        assertTrue(subject.contains(object1));
+        assertTrue(subject.contains(object2));
+    }
+
+    @Test
+    public void RemoveManyItems() {
+        Object object1 = new Object();
+        Object object2 = new Object();
+        Object object3 = new Object();
+        subject.add(object1);
+        subject.add(object2);
+        subject.add(object3);
+        subject.remove(object1);
+        subject.remove(object2);
+
+        assertFalse(subject.contains(object1));
+        assertFalse(subject.contains(object2));
+        assertTrue(subject.contains(object3));
+    }
 }
