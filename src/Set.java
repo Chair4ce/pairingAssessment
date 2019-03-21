8d3715fbd941f6e72d21eb486c96b9d9008543c9
Jason Rimer <jasonmrimer@gmail.com>
2019-03-21 05:42:20 -1000

    Step 8 - Refactor emptiness, contains, null check

--- src/Set.java
+++ src/Set.java
@@ -1,32 +1,29 @@
 public class Set {
-    private boolean emptiness = true;
     private Object[] container = new Object[10];
     private int count = 0;
 
     public boolean isEmpty() {
-        return this.emptiness;
+        return count == 0;
     }
 
     public void add(Object object) {
         container[count] = object;
         count++;
-        this.emptiness = false;
     }
 
     public void remove(Object object) {
         if (this.contains(object)) {
             this.container[0] = null;
-            this.emptiness = true;
+            count--;
         }
     }
 
     public boolean contains(Object object) {
-        boolean doesContain = false;
         for (int index = 0; index < count; index++) {
-            if (container[index] != null && container[index].equals(object)) {
-                doesContain = true;
+            if (container[index].equals(object)) {
+                return true;
             }
         }
-        return doesContain;
+        return false;
     }
 }
