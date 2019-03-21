45a2d2ea158288e13b065554787449d1a2480969
Jason Rimer <jasonmrimer@gmail.com>
2019-03-21 11:13:07 -1000

    Interview with Patrick Shea 20190321

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,24 @@
 public class Set {
-    
+    private boolean emptiness = true;
+    private Object[] container = new Object[10];
+    private int count = 0;
+
+    public boolean isEmpty() {
+        return emptiness;
+    }
+
+    public void add(Object object) {
+        container[count] = object;
+        count++;
+        emptiness = false;
+    }
+
+    public boolean contains(Object object) {
+        for (int index = 0; index < count; index++) {
+            if (container[index].equals(object)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/SetTest.java
+++ src/SetTest.java
@@ -5,5 +5,34 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SetTest {
+    @Test
+    public void NewSetIsEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty());
+    }
 
+    @Test
+    public void AddObject() {
+        Set parkingGarage = new Set();
+        Object car = new Object();
+        parkingGarage.add(car);
+        assertFalse(parkingGarage.isEmpty());
+        assertTrue(parkingGarage.contains(car));
+    }
+
+    @Test
+    public void AddManyObjects() {
+        Set parkingGarage = new Set();
+        Object car = new Object();
+        Object truck = new Object();
+        Object van = new Object();
+        parkingGarage.add(car);
+        parkingGarage.add(truck);
+        parkingGarage.add(van);
+
+        assertFalse(parkingGarage.isEmpty());
+        assertTrue(parkingGarage.contains(car));
+        assertTrue(parkingGarage.contains(truck));
+        assertTrue(parkingGarage.contains(van));
+    }
 }
