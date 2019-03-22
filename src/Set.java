01dba68746cd9f18daebe1b2ce265f6da00cb808
Jason Rimer <jasonmrimer@gmail.com>
2019-03-21 17:01:16 -1000

    Interview Alec Logalog 20190320

--- src/Set.java
+++ src/Set.java
@@ -1,3 +1,36 @@
 public class Set {
-    
+
+    Object[] container = new Object[1000];
+    int spot = 0;
+
+    public boolean isEmpty() {
+        return spot==0;
+    }
+
+    public void add(Object object) {
+        container[spot] = object;
+        spot++;
+    }
+
+    public void remove(Object object) {
+
+        for (int i = 0; i < spot; i++) {
+            if (container[i].equals(object)) {
+                container[i]= null;
+                spot--;
+            }
+        }
+
+    }
+
+    public boolean contains(Object object) {
+        if (container.equals(object)) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
+
+
--- src/SetTest.java
+++ src/SetTest.java
@@ -6,4 +6,47 @@ import static org.junit.Assert.assertFalse;
 
 public class SetTest {
 
+    @Test
+    public void newEmpty() {
+        Set subject = new Set();
+        assertTrue(subject.isEmpty());
+    }
+
+    @Test
+    public void addNotEmpty() {
+        Set subject = new Set();
+        subject.add(new Object());
+        assertFalse(subject.isEmpty());
+    }
+
+    @Test
+    public void removeEmpty() {
+        Set subject = new Set();
+        Object chickennuggets = new Object();
+        subject.add(chickennuggets);
+        subject.remove(chickennuggets);
+        assertTrue(subject.isEmpty());
+    }
+
+    @Test
+    public void addContains() {
+        Set subject = new Set();
+        Object chnug = new Object();
+        subject.add(chnug);
+        assertTrue(subject.contains(chnug));
+    }
+
+    @Test
+    public void addManyContains() {
+        Set subject = new Set();
+        Object chnug = new Object();
+        Object burger = new Object();
+        subject.add(chnug);
+        subject.add(burger);
+        assertTrue(subject.contains(chnug));
+        assertTrue(subject.contains(burger));
+    }
+
+    //chicken nugget and burger but only care about burger
+
 }
